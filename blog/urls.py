10a7054b6dfd5f3e1212470bffacba0f3c7e3cd8
Pawlo <pawlonazaruk@gmail.com>
2020-09-11 18:12:43 +0200

    db + register

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 
+
     path('benon/', views.benon, name='blog-benon'),
     path('benon/asd/', views.benon_asd, name='blog-benon-asd'),
 ]
