6965401d987153ecd7a90536f0c0ed6141ad9a08
Pawlo <pawlonazaruk@gmail.com>
2020-09-17 23:05:08 +0200

    profile update

--- users/signals.py
+++ users/signals.py
@@ -7,6 +7,7 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+        print(f'Signal/create_profile - instance: {instance}')
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
